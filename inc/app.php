eb16f13a36b03955204b17f7c013b4291010697d
Darklg <kev.rocher@gmail.com>
2014-11-16 19:18:40 +0100

    Basic API

--- inc/app.php
+++ inc/app.php
@@ -4,4 +4,8 @@ $html_posted = '';
 if (!empty($_POST) && isset($_POST['html'])) {
     $html_posted = $_POST['html'];
     $html2css->parse_html($html_posted);
+    if (isset($_POST['api'])) {
+        echo $html2css->generateCSS();
+        die;
+    }
 }
